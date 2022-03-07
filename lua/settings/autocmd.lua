4c8728a15b60d19bc4eec013c3a33963923a1d3c
tknightz <hatuyen2699@gmail.com>
2022-03-08 00:03:05 +0700

    update autocmd for terminal

--- lua/settings/autocmd.lua
+++ lua/settings/autocmd.lua
@@ -6,10 +6,9 @@ vim.api.nvim_command('augroup END')
 
 
 vim.api.nvim_command('augroup terminal_settings')
--- vim.api.nvim_command('autocmd WinEnter term://* startinsert')
-vim.api.nvim_command('autocmd TermEnter * startinsert')
+vim.api.nvim_command('autocmd WinEnter term://* startinsert')
 vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber signcolumn=no')
-vim.api.nvim_command('autocmd TermClose * :call feedkeys("i")')
+vim.api.nvim_command('autocmd TermClose term://* :call nvim_input("<CR>")')
 vim.api.nvim_command('augroup END')
 
 
