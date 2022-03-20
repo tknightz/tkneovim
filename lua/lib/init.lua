7f5ecc2a68e0a1e7052cb0a1016f812046e290e7
tknightz <hatuyen2699@gmail.com>
2022-03-20 19:42:37 +0700

    add function load_module

--- lua/lib/init.lua
+++ lua/lib/init.lua
@@ -16,4 +16,25 @@ M.build_init_module = function(path)
   return exported_module
 end
 
+
+M.load_module = function(name)
+  vim.cmd("packadd ".. name)
+
+  local home_config = vim.fn.expand('~') .. '/.config/nvim/lua/'
+  local setup_path = 'modules/setup/' .. name
+  local config_path = 'modules/config/' .. name
+
+  local is_existed_setup_file = vim.fn.filereadable(home_config .. setup_path .. '.lua') + vim.fn.isdirectory(home_config .. setup_path)
+  local is_existed_config_file = vim.fn.filereadable(home_config .. config_path .. '.lua') + vim.fn.isdirectory(home_config .. config_path)
+
+  if is_existed_setup_file ~= 0 then
+      vim.cmd("lua require('" .. setup_path .. "')")
+  end
+
+  if is_existed_config_file ~= 0 then
+      vim.cmd("lua require('" .. config_path .. "')")
+  end
+
+end
+
 return M
