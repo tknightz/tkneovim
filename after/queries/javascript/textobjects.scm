b49ea30329ccd603d5cf8f54df994bd4aaf2a987
tknightz <hatuyen2699@gmail.com>
2024-03-07 14:19:10 +0700

    chore: enhance movement by textobjects

---
+++ after/queries/javascript/textobjects.scm
@@ -0,0 +1,5 @@
+;; extends
+(method_definition (property_identifier)) @method.def
+(function_declaration (identifier)) @method.def
+(method_definition (formal_parameters (identifier) @parameter.def))
+(function_declaration (formal_parameters (identifier) @parameter.def))
---
+++ after/queries/typescript/textobjects.scm
@@ -0,0 +1,5 @@
+;; extends
+(method_definition (property_identifier) @method.def)
+(function_declaration (identifier) @method.def)
+(method_definition (formal_parameters (required_parameter) @parameter.def))
+(function_declaration (formal_parameters (required_parameter) @parameter.def))
